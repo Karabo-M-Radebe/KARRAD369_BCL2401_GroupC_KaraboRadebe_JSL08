e9afb96cc55eb42f4b2b21c7b082dcddeb8c6acd
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-04-09 15:44:58 +0200

    methods for BankBranch commit

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ class BankBranch {
         }
         return bankBranchInstance;
     }
-
 }
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
-
+get.BranchInfo(){
+    return this.branchInfo;
+}
 // 5. In the usage section:
 //    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
 //    - Use the `getBranchInfo` method to retrieve branch information from the instances.
